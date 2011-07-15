cecc90c7f38a2271fa6ed1ae1b8a29da5e03cf77
Andrew Premdas <apremdas@yahoo.co.uk>
2011-07-15 15:33:51 +0100

    more radian6 spec
    - extracted Radian6SpecHelper
    - used fixture in above spec helper in Post spec

--- spec/lib/radian6/api_spec.rb
+++ spec/lib/radian6/api_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'radian6'
 
 describe Radian6::API do
+  include Radian6SpecHelper
   before do
     stub_request(:get, "http://api.radian6.com/socialcloud/v1/auth/authenticate").
       to_return(:status => 200, :body => auth_xml)
@@ -146,83 +147,7 @@ describe Radian6::API do
 
            counter.should == 1
          end
-       end
-      
+       end     
     end  
   end  
-  
-  def auth_xml
-    "<auth><token>abcdefghi</token></auth>"
-  end
-
-  def topics_xml
-    '<topicFilters>
-      <topicFilter>
-        <name><![CDATA[FILTER TEST]]></name>
-        <topicFilterId>111111</topicFilterId>
-        <filterGroups>
-          <filterGroup>
-            <filterGroupId>222222</filterGroupId>
-            <name><![CDATA[Group 2 test]]></name>
-            <filterQueries>
-              <filterQuery>
-                <query><![CDATA["apple" AND "banana"]]></query>
-                <filterQueryId>333333</filterQueryId>
-                <filterQueryTypeId>0</filterQueryTypeId>
-              </filterQuery>
-              <filterQuery>
-                <query><![CDATA["coconut" AND "date"]]></query>
-                <filterQueryId>444444</filterQueryId>
-                <filterQueryTypeId>0</filterQueryTypeId>
-              </filterQuery>
-            </filterQueries>
-          </filterGroup>
-          <filterGroup>
-            <filterGroupId>555555</filterGroupId>
-            <name><![CDATA[Group 5 test]]></name>
-            <filterQueries>
-              <filterQuery>
-                <query><![CDATA["elderberry" AND "fig"]]></query>
-                <filterQueryId>666666</filterQueryId>
-                <filterQueryTypeId>0</filterQueryTypeId>
-              </filterQuery>
-            </filterQueries>
-          </filterGroup>
-      </filterGroups>
-      </topicFilter>
-    </topicFilters>'
-  end
-
-  def recent_xml
-    '<radian6_RiverOfNews_export>
-      <article ID="111111">
-        <description charset="UTF-8">
-          <headline><![CDATA[TEST HEADLINE 1]]></headline>
-          <author fbid="-1"><![CDATA[author]]></author>
-          <content><![CDATA[article content]]></content>
-        </description>
-        <article_url><![CDATA[http://example.com/111111]]></article_url>
-        <media_provider id="10">TWITTER</media_provider>
-        <publish_date epoch="1308738914000">Jun 22, 2011 11:35 AM</publish_date>
-      </article>
-      <article ID="222222">
-        <description charset="UTF-8">
-          <headline><![CDATA[TEST HEADLINE 2]]></headline>
-          <author fbid="-1"><![CDATA[author]]></author>
-          <content><![CDATA[article content]]></content>
-        </description>
-        <article_url><![CDATA[http://example.com/222222]]></article_url>
-        <media_provider id="10">TWITTER</media_provider>
-        <publish_date epoch="1308738914000">Jun 22, 2011 11:35 AM</publish_date>
-      </article>
-    </radian6_RiverOfNews_export>'
-  end
-  alias :range_xml :recent_xml
-
-  def pages_xml(article_count=10, total_article_count=100)
-    "<radian6_RiverOfNews_export>
-      <article_count>#{article_count}</article_count>
-      <total_article_count>#{total_article_count}</total_article_count>
-    </radian6_RiverOfNews_export>"
-  end
 end
--- spec/lib/radian6/post_spec.rb
+++ spec/lib/radian6/post_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
+require 'radian6'
 
 describe Radian6::Post do
+  include Radian6SpecHelper
+  post_attributes = %w{author avatar username title created_at updated_at id body source permalink}
   describe ".from_xml" do
     context "nil parameter" do
       it 'should return a collection' do
@@ -24,14 +27,22 @@ describe Radian6::Post do
       it 'should return a collection' do
         Radian6::Post.from_xml('').should respond_to :each
       end
-    end              
+    end  
+    
+    context "proccesing recent_xml" do
+      it "should produce Radian6::Posts" do
+        Radian6::Post.from_xml(recent_xml).each do |post| 
+          post.is_a?(Radian6::Post).should be true       
+        end
+      end
+    end
   end
   
   describe "initialization" do  
     ['Radian6::Post.new({})', 'Radian6::Post.new(nil)', 'Radian6::Post.new([])'].each do |subj|
       context "initializing with #{subj}" do
         subject { eval subj}
-        %w{author avatar username title created_at updated_at id body source permalink}.each do |method|
+        post_attributes.each do |method|
           its(method.to_sym) { should be_empty }
         end
       end
