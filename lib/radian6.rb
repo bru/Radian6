bcb3efcb1e4574bd27e2bf90e1daa7f695be7871
Richard Headley <richard.headley@headshift.com>
2011-07-26 17:12:12 +0100

    You can now show 'more' messages (conversations) on the dashboard and individual topic pages [#16016887]

--- lib/radian6.rb
+++ lib/radian6.rb
@@ -1,7 +1,8 @@
 require 'radian6/api'
 require 'radian6/post'
-require 'radian6/SAX/post_counter'
-require 'radian6/SAX/post'
+require 'radian6/sax/post_counter'
+require 'radian6/sax/post'
 require 'radian6/topic'
 require 'radian6/filter_query'
 require 'radian6/filter_group'
+
