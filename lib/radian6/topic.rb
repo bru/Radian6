fe405ea168637a218645b4483dafaaf3b584f73f
Riccardo Cambiassi <bru@codewitch.org>
2011-08-02 09:57:39 +0100

    restructured radian6/topic a bit to boost readability

--- lib/radian6/topic.rb
+++ lib/radian6/topic.rb
@@ -12,32 +12,40 @@ module Radian6
     def self.from_xml(xml)
       xml_topics = Nokogiri::XML(xml).root.xpath('//topicFilters/topicFilter') rescue []
       topics = []
-      xml_topics.each_with_index do |xml_topic, index|
-        groups = []
-        xml_topic.xpath('./filterGroups/filterGroup').each do |xml_group|
-          queries = []
-          xml_group.xpath('./filterQueries/filterQuery').each do |xml_query|
-            queries << FilterQuery.new({
-              :id             => xml_query.xpath('./filterQueryId').text,
-              :query          => xml_query.xpath('./query').text,
-              :isExcludeQuery => xml_query.xpath('./filterQueryTypeId').text
-            })
-          end
-          
-          groups << FilterGroup.new({
-            :name     => xml_group.xpath('./name').text,
-            :id       => xml_group.xpath('./filterGroupId').text,
-            :queries  => queries
-          })
-        end
-        topic = self.new({
-          :name     => xml_topic.xpath('./name').text,
-          :id       => xml_topic.xpath('./topicFilterId').text,
-          :groups   => groups
-        })
-        topics << topic
+      xml_topics.each do |xml_topic|
+        topics << parse_topic(xml_topic)
       end
       return topics
+    end
+    private
+
+    def self.parse_topic(xml_topic)
+      groups = []
+      xml_topic.xpath('./filterGroups/filterGroup').each do |xml_group|
+        groups << parse_group(xml_group)
+      end
+      self.new({
+        :name     => xml_topic.xpath('./name').text,
+        :id       => xml_topic.xpath('./topicFilterId').text,
+        :groups   => groups
+      })
+    end
+
+    def self.parse_group(xml_group)
+      queries = []
+      xml_group.xpath('./filterQueries/filterQuery').each do |xml_query|
+        queries << FilterQuery.new({
+          :id             => xml_query.xpath('./filterQueryId').text,
+          :query          => xml_query.xpath('./query').text,
+          :isExcludeQuery => xml_query.xpath('./filterQueryTypeId').text
+        })
+      end
+
+      FilterGroup.new({
+        :name     => xml_group.xpath('./name').text,
+        :id       => xml_group.xpath('./filterGroupId').text,
+        :queries  => queries
+      })
     end
   end
 end
