76f22ab4136e7e890604dc771d10ce54608cc365
Alessandro Morandi <alessandro@headshift.com>
2011-10-28 17:35:14 +0100

    Refactoring: removed LDAP integration (no longer used).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -57,7 +57,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  # config.use_transactional_fixtures = true
 
 
   config.before(:suite) do
@@ -81,7 +81,6 @@ RSpec.configure do |config|
   end
 end
 
-include Testing::Ldap
 include Testing::LoginMethods
 
 
