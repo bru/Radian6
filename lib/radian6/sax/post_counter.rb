fba4a899b7d86b5c7a33e06e8c87c737782024f8
Riccardo Cambiassi <bru@codewitch.org>
2012-01-19 12:35:47 +0000

    Speed up article counting from raw radian6 River Of News XML

--- lib/radian6/sax/post_counter.rb
+++ lib/radian6/sax/post_counter.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Radian6
   module SAX
     class PostCounter < Nokogiri::XML::SAX::Document
@@ -8,41 +10,51 @@ module Radian6
         @in_error = false
       end
       def start_element name, attrs = []
-        case name
-        when "article_count"
-          @in_count = true
-        when "total_article_count"
-          @in_total = true
-        when "error"
-          @in_error = true
-        when "publish_date"
-          fetch_timestamp(attrs)
+        unless done?
+          case name
+          when "article_count"
+            @in_count = true
+          when "total_article_count"
+            @in_total = true
+          when "error"
+            @in_error = true
+          when "publish_date"
+            fetch_timestamp(attrs)
+          end
         end
       end
 
       def end_element name
-        case name
-        when "article_count"
-          @in_count = false
-        when "total_article_count"
-          @in_total = false
-        when "error"
-          @in_error
+        unless done?
+          case name
+          when "article_count"
+            @in_count = false
+          when "total_article_count"
+            @in_total = false
+          when "error"
+            @in_error = false
+          end
         end
       end
 
       def characters text
-        if @in_count
-          @count = text.to_i
-        elsif @in_total
-          @total = text.to_i
-        elsif @in_error
-          @error = text
+        unless done?
+          if @in_count
+            @count = text.to_i
+          elsif @in_total
+            @total = text.to_i
+          elsif @in_error
+            @error = text
+          end
         end
       end
 
       def fetch_timestamp(attrs = [])
         @last_timestamp ||= attrs.select { |key, value| key == "epoch" }.first[1].to_i
+      end
+
+      def done?
+        (@count.nil? || @total.nil? || @last_timestamp.nil?) ? false : true
       end
     end
   end
